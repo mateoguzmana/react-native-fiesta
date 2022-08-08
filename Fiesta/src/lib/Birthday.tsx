99740dab7857e2f702032b4893890be65df4b82d
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2022-08-09 00:46:17 +0200

    Improving birthday component.

--- Fiesta/src/lib/Birthday.tsx
+++ Fiesta/src/lib/Birthday.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import {Canvas} from '@shopify/react-native-skia';
 import {StyleSheet} from 'react-native';
 import Balloon from './Balloon';
@@ -9,28 +9,28 @@ interface BirthdayProps {
   theme?: string[];
 }
 
-function Birthday({theme = FiestaThemes.dark}: BirthdayProps) {
-  const X_GAP = 60;
-  const yPositions = [150, 0, 300, 100, 200, 0, 200, 100, 300, 0];
-  const possibleRadius = [30, 35, 40, 45];
+const X_GAP = 60;
+const yPositions = [150, 0, 300, 100, 200, 0, 200, 100, 300, 0];
+const possibleRadius = [30, 35, 40, 45];
+const optimalNumberOfBalloons = Math.floor(screenWidth / X_GAP);
+const ballonsToRenderArray = [...Array(optimalNumberOfBalloons)];
 
-  const optimalNumberOfBalloons = Math.floor(screenWidth / X_GAP);
+function Birthday({theme = FiestaThemes.default}: BirthdayProps) {
+  const randomisedColors = useMemo(() => shuffleArray(theme), [theme]);
 
-  const randomisedColors = shuffleArray(theme);
-
-  const renderBalloons = () => {
-    return [...Array(optimalNumberOfBalloons)].map((_, index) => (
-      <Balloon
-        key={Math.random()}
-        x={X_GAP * index}
-        y={yPositions[index]}
-        color={randomisedColors[index]}
-        r={possibleRadius[Math.floor(Math.random() * possibleRadius.length)]}
-      />
-    ));
-  };
-
-  return <Canvas style={styles.canvas}>{renderBalloons()}</Canvas>;
+  return (
+    <Canvas style={styles.canvas}>
+      {ballonsToRenderArray.map((_, index) => (
+        <Balloon
+          key={Math.random()}
+          x={X_GAP * index}
+          y={yPositions[index]}
+          color={randomisedColors[index]}
+          r={possibleRadius[Math.floor(Math.random() * possibleRadius.length)]}
+        />
+      ))}
+    </Canvas>
+  );
 }
 
 const styles = StyleSheet.create({
