33523214fa55421aa969fd16822f5cbb87e20361
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2022-08-13 04:45:54 +0200

    docs(saurus): cleanup

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -66,7 +66,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'intro',
+            docId: 'getting-started/installation',
             position: 'left',
             label: 'Docs',
           },
@@ -85,7 +85,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/docs/getting-started/installation',
               },
             ],
           },
