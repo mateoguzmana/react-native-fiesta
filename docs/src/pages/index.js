1af909b3e13cb24751fcd3d4fa874e87d57b33d5
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2022-08-13 09:08:29 +0200

    chore(docs): adding proper order and also fixing category entry

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/getting-started"
+            to="/docs/category/getting-started"
           >
             Get Started
           </Link>
