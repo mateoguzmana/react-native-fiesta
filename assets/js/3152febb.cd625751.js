d3dc02f452cb8e6a15ff3572e566b33e614914e6
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2022-08-14 22:26:22 +0200

    Deploy website - based on f979fd66750524d4027c67a1b002a3bac3eb5a65

---
+++ assets/js/3152febb.cd625751.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[225],{3492:t=>{t.exports=JSON.parse('{"title":"Getting Started","slug":"/category/getting-started","permalink":"/docs/category/getting-started","navigation":{"next":{"title":"Installation","permalink":"/docs/getting-started/installation"}}}')}}]);
---
+++ assets/js/64566cc6.29c23954.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[555],{6547:e=>{e.exports=JSON.parse('{"title":"Components","slug":"/category/components","permalink":"/docs/category/components","navigation":{"previous":{"title":"First Fiesta","permalink":"/docs/getting-started/first-fiesta"},"next":{"title":"Balloons","permalink":"/docs/components/balloons"}}}')}}]);
