4bc8ef540c49a456bd707f8ad170cc78f73d4d9b
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2022-08-16 21:13:13 +0200

    feat(balloon): adding initial configuration for balloon string movement

--- src/components/Balloon.tsx
+++ src/components/Balloon.tsx
@@ -1,5 +1,13 @@
 import React, { memo } from 'react';
-import { Circle, Group, Oval, Path, Shadow } from '@shopify/react-native-skia';
+import {
+  Circle,
+  Group,
+  Oval,
+  Path,
+  Shadow,
+  useComputedValue,
+  useTiming,
+} from '@shopify/react-native-skia';
 import { palette } from '../constants/theming';
 
 interface BalloonProps {
@@ -17,40 +25,58 @@ function Balloon({
   r = 40,
   depth = 1,
 }: BalloonProps) {
+  const stringSkewX = useTiming({
+    to: 0.05,
+    from: -0.05,
+    yoyo: true,
+    loop: true,
+  });
+
+  const stringRotationTransform = useComputedValue(
+    () => [
+      {
+        skewX: stringSkewX.current,
+      },
+    ],
+    [stringSkewX]
+  );
+
   return (
-    <Group transform={[{ scale: depth }]}>
-      <Group transform={[{ translateX: x }]}>
-        <Group
-          transform={[
-            {
-              translateY: y,
-            },
-          ]}
-        >
-          <Group transform={[{ translateX: -55, skewX: 0.1 }]}>
-            <Path
-              path={`M 100 22 C 90 10, 110 80, 100 100 S 100 170, 100 150`}
-              color={palette.golden}
-              style="stroke"
-              strokeJoin="round"
-              strokeWidth={5}
+    <Group transform={stringRotationTransform}>
+      <Group transform={[{ scale: depth }]}>
+        <Group transform={[{ translateX: x }]}>
+          <Group
+            transform={[
+              {
+                translateY: y,
+              },
+            ]}
+          >
+            <Group transform={[{ translateX: -55 }]}>
+              <Path
+                path={`M 100 22 C 90 10, 110 80, 100 100 S 100 170, 100 150`}
+                color={palette.golden}
+                style="stroke"
+                strokeJoin="round"
+                strokeWidth={5}
+              />
+            </Group>
+
+            <Group transform={[{ translateY: -50 }]}>
+              <Group>
+                <Oval height={r * 2.3} width={r * 2} color={color} />
+                <Shadow dx={-12} dy={-12} blur={40} color="#fff" inner />
+              </Group>
+            </Group>
+
+            <Circle
+              cx={r / 0.8}
+              cy={-25}
+              r={r / 4}
+              color={palette.gray}
+              opacity={0.3}
             />
           </Group>
-
-          <Group transform={[{ translateY: -50 }]}>
-            <Group>
-              <Oval height={r * 2.3} width={r * 2} color={color} />
-              <Shadow dx={-12} dy={-12} blur={40} color="#fff" inner />
-            </Group>
-          </Group>
-
-          <Circle
-            cx={r / 0.8}
-            cy={-25}
-            r={r / 4}
-            color={palette.gray}
-            opacity={0.3}
-          />
         </Group>
       </Group>
     </Group>
