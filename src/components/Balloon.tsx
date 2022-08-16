cfbba1f2ce871fec02701d1bb81b9e8455a8e651
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2022-08-16 21:15:28 +0200

    feat(balloon): limiting skewX movement to balloon string instead of whole balloon

--- src/components/Balloon.tsx
+++ src/components/Balloon.tsx
@@ -42,16 +42,16 @@ function Balloon({
   );
 
   return (
-    <Group transform={stringRotationTransform}>
-      <Group transform={[{ scale: depth }]}>
-        <Group transform={[{ translateX: x }]}>
-          <Group
-            transform={[
-              {
-                translateY: y,
-              },
-            ]}
-          >
+    <Group transform={[{ scale: depth }]}>
+      <Group transform={[{ translateX: x }]}>
+        <Group
+          transform={[
+            {
+              translateY: y,
+            },
+          ]}
+        >
+          <Group transform={stringRotationTransform}>
             <Group transform={[{ translateX: -55 }]}>
               <Path
                 path={`M 100 22 C 90 10, 110 80, 100 100 S 100 170, 100 150`}
@@ -61,22 +61,22 @@ function Balloon({
                 strokeWidth={5}
               />
             </Group>
-
-            <Group transform={[{ translateY: -50 }]}>
-              <Group>
-                <Oval height={r * 2.3} width={r * 2} color={color} />
-                <Shadow dx={-12} dy={-12} blur={40} color="#fff" inner />
-              </Group>
-            </Group>
-
-            <Circle
-              cx={r / 0.8}
-              cy={-25}
-              r={r / 4}
-              color={palette.gray}
-              opacity={0.3}
-            />
           </Group>
+
+          <Group transform={[{ translateY: -50 }]}>
+            <Group>
+              <Oval height={r * 2.3} width={r * 2} color={color} />
+              <Shadow dx={-12} dy={-12} blur={40} color="#fff" inner />
+            </Group>
+          </Group>
+
+          <Circle
+            cx={r / 0.8}
+            cy={-25}
+            r={r / 4}
+            color={palette.gray}
+            opacity={0.3}
+          />
         </Group>
       </Group>
     </Group>
