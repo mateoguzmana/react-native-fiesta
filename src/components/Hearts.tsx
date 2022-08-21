612d035b3a7a78548ec6afcca2d32ed67532dcc6
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2022-08-21 11:38:27 +0200

    chore(hearts): refactoring hearts to use popper component

--- src/components/Hearts.tsx
+++ src/components/Hearts.tsx
@@ -1,23 +1,12 @@
-import React, { memo, useCallback, useEffect, useMemo } from 'react';
-import { StyleSheet } from 'react-native';
-import {
-  Canvas,
-  Group,
-  runSpring,
-  useComputedValue,
-  useValue,
-} from '@shopify/react-native-skia';
-import { screenHeight } from '../constants/dimensions';
+import React, { memo, useMemo } from 'react';
 import { screenWidth } from '../constants/dimensions';
 import Heart from './Heart';
-import { shuffleArray } from '../utils/array';
 import { colorsFromTheme } from '../utils/colors';
 import { FiestaThemes } from '../constants/theming';
+import Popper from './Popper';
 
 const xGap = 40;
 const optimalNumberOfHearts = Math.floor(screenWidth / xGap);
-const heartsToRenderArray = [...Array(optimalNumberOfHearts)];
-const yPositions = shuffleArray(heartsToRenderArray.map((_, i) => i * xGap));
 
 export interface HeartsProps {
   theme?: string[];
@@ -28,54 +17,14 @@ function Hearts({ theme = FiestaThemes.default }: HeartsProps) {
     () => colorsFromTheme(theme, optimalNumberOfHearts),
     [theme]
   );
-  const yPosition = useValue(screenHeight);
-
-  const changeItemPosition = useCallback(
-    () =>
-      runSpring(yPosition, -screenHeight, {
-        stiffness: 0.5,
-      }),
-    [yPosition]
-  );
-
-  const transform = useComputedValue(
-    () => [
-      {
-        translateY: yPosition.current,
-      },
-    ],
-    [yPosition]
-  );
-
-  useEffect(() => {
-    changeItemPosition();
-  }, [changeItemPosition]);
 
   return (
-    <Canvas style={styles.canvas}>
-      <Group transform={transform}>
-        {heartsToRenderArray.map((_, index) => (
-          <Heart
-            key={index}
-            x={xGap * index}
-            y={yPositions[index]}
-            color={colors[index]}
-          />
-        ))}
-      </Group>
-    </Canvas>
+    <Popper
+      renderItem={({ x, y }, index) => (
+        <Heart key={index} x={x} y={y} color={colors[index]} />
+      )}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  canvas: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    zIndex: -1,
-  },
-});
 
 export default memo(Hearts);
