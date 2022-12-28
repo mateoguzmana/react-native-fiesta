c9e4d60503cae51161f5a8e2d167c43b17dd4980
Mateo Guzmán <mateo.guzmanalvarez@gmail.com>
2022-12-28 21:41:25 +0100

    refactor(components): using matrix instead of multiple groups for hearts and stars

--- src/components/Heart.tsx
+++ src/components/Heart.tsx
@@ -1,5 +1,9 @@
-import React, { memo, useCallback, useEffect } from 'react';
-import { Group, Path, runSpring, useValue } from '@shopify/react-native-skia';
+import React, { memo, useMemo } from 'react';
+import {
+  Path,
+  processTransform2d,
+  useSpring,
+} from '@shopify/react-native-skia';
 import { baseColors } from '../constants/theming';
 import { singleItemFadeSpeed } from '../constants/speed';
 
@@ -12,30 +16,28 @@ export interface HeartProps {
 
 export const Heart = memo(
   ({ x, y, autoplay = true, color = baseColors.red }: HeartProps) => {
-    const opacity = useValue(1);
-
-    const changeOpacity = useCallback(
-      () => runSpring(opacity, 0, singleItemFadeSpeed),
-      [opacity]
+    const opacity = useSpring(
+      { to: autoplay ? 0 : 1, from: 1 },
+      singleItemFadeSpeed
     );
 
-    useEffect(() => {
-      autoplay && changeOpacity();
-    }, [autoplay, changeOpacity]);
+    const matrix = useMemo(
+      () =>
+        processTransform2d([
+          { translateX: x },
+          { translateY: y },
+          { scale: 0.05 },
+        ]),
+      [x, y]
+    );
 
     return (
-      // @TODO: this can be optimised by using a matrix instead of multiple transforms
-      <Group transform={[{ translateY: y }]}>
-        <Group transform={[{ translateX: x }]}>
-          <Group transform={[{ scale: 0.05 }]}>
-            <Path
-              path="m263.42 235.15c-66.24 0-120 53.76-120 120 0 134.76 135.93 170.09 228.56 303.31 87.574-132.4 228.56-172.86 228.56-303.31 0-66.24-53.76-120-120-120-48.048 0-89.402 28.37-108.56 69.188-19.161-40.817-60.514-69.188-108.56-69.188z"
-              color={color}
-              opacity={opacity}
-            />
-          </Group>
-        </Group>
-      </Group>
+      <Path
+        path="m263.42 235.15c-66.24 0-120 53.76-120 120 0 134.76 135.93 170.09 228.56 303.31 87.574-132.4 228.56-172.86 228.56-303.31 0-66.24-53.76-120-120-120-48.048 0-89.402 28.37-108.56 69.188-19.161-40.817-60.514-69.188-108.56-69.188z"
+        color={color}
+        opacity={opacity}
+        matrix={matrix}
+      />
     );
   }
 );
