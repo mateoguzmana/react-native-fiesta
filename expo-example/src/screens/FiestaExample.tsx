9c4787e57ca58bb4e0a12285fe0387b74d3c115b
Mateo Guzmán <info@mateoguzman.net>
2024-06-01 00:02:43 +0200

    fix(examples): putting back previous example

--- expo-example/src/screens/FiestaExample.tsx
+++ expo-example/src/screens/FiestaExample.tsx
@@ -1,69 +1,66 @@
-import React from 'react';
-import {
-  Canvas,
-  LinearGradient,
-  Fill,
-  // Use this function instead of interpolateColor from Reanimated
-  interpolateColors,
-  vec,
-  Group,
-} from '@shopify/react-native-skia';
-import { useEffect } from 'react';
-import { useWindowDimensions } from 'react-native';
-import {
-  useDerivedValue,
-  useSharedValue,
-  withRepeat,
-  withTiming,
-} from 'react-native-reanimated';
+import React, { useCallback } from 'react';
+import { Dimensions, StyleSheet, View } from 'react-native';
+import { Firework, Fireworks } from 'react-native-fiesta';
+import { Canvas } from '@shopify/react-native-skia';
+import { type RouteProp, useRoute } from '@react-navigation/native';
+import type { RootStackParamList } from '../App';
 
-const startColors = [
-  'rgba(34, 193, 195, 0.4)',
-  'rgba(34,193,195,0.4)',
-  'rgba(63,94,251,1)',
-  'rgba(253,29,29,0.4)',
-];
-const endColors = [
-  'rgba(0,212,255,0.4)',
-  'rgba(253,187,45,0.4)',
-  'rgba(252,70,107,1)',
-  'rgba(252,176,69,0.4)',
-];
+type FiestaExampleScreenNavigationProp = RouteProp<
+  RootStackParamList,
+  'FiestaExample'
+>;
 
-export const FiestaExample = () => {
-  const { width, height } = useWindowDimensions();
-  const colorsIndex = useSharedValue(0);
-  const opacity = useSharedValue(0);
-  useEffect(() => {
-    colorsIndex.value = withRepeat(
-      withTiming(startColors.length - 1, {
-        duration: 4000,
-      }),
-      -1,
-      true
-    );
-    opacity.value = withTiming(1, {
-      duration: 100,
-    });
-  }, [colorsIndex, opacity]);
-
-  const gradientColors = useDerivedValue(() => {
-    return [
-      interpolateColors(colorsIndex.value, [0, 1, 2, 3], startColors),
-      interpolateColors(colorsIndex.value, [0, 1, 2, 3], endColors),
-    ];
-  });
-  return (
-    <Canvas style={{ flex: 1, backgroundColor: 'black' }}>
-      <Group opacity={opacity}>
-        <Fill>
-          <LinearGradient
-            start={vec(0, 0)}
-            end={vec(width, height)}
-            colors={gradientColors}
-          />
-        </Fill>
-      </Group>
-    </Canvas>
-  );
+const initialPosition = {
+  x: Dimensions.get('window').width / 2,
+  y: Dimensions.get('window').width / 2,
 };
+
+export function FiestaExample() {
+  const route = useRoute<FiestaExampleScreenNavigationProp>();
+
+  const renderAnimation = useCallback(() => {
+    switch (route.params.example) {
+      case 'Firework':
+        return (
+          <Canvas style={styles.canvas}>
+            <Firework
+              color="rgba(179, 173, 196, 0.4)"
+              initialPosition={initialPosition}
+              fireworkRadius={Dimensions.get('window').width}
+              autoHide={false}
+            />
+          </Canvas>
+        );
+      case 'Fireworks':
+        return (
+          <Fireworks
+            fireworkRadius={Dimensions.get('window').width}
+            autoHide={false}
+          />
+        );
+      case 'CrazyFireworks':
+        return (
+          <Fireworks
+            autoHide={false}
+            numberOfFireworks={3}
+            numberOfParticles={80}
+            fireworkRadius={2000}
+          />
+        );
+      default:
+        return null;
+    }
+  }, [route.params.example]);
+
+  return <View style={styles.container}>{renderAnimation()}</View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#181D31',
+  },
+  canvas: {
+    flex: 1,
+  },
+});
